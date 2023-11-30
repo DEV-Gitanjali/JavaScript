502d96ed2ab2eb8862f57af24470e9c50d0bbba3
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-11-30 11:26:06 +0530

    this code for user take input

---
+++ text1.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+
+
+var a =prompt("enter first name");
+var b=prompt("enter second name");
+var c=a+b;
+console.log("full name is:",c)
+
+// console.log("hello subham");
