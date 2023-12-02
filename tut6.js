293bac3a0b859a54541d77e05f4f065949e442b2
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-02 21:33:20 +0530

    this code for switch case

--- tut6.js
+++ tut6.js
@@ -1,22 +1,25 @@
 
 
-let  a = prompt("hey what you age ?");
-a=Number.parseInt(a);
+// let  a = prompt("hey what you age ?");
+// a=Number.parseInt(a);
 
-console.log(typeof a);
+// console.log(typeof a);
+ let a=20;
 
-if(a<0){
-    alert("this is valid age")
-}
+// if(a<0){
+//     alert("this is valid age")
+// }
 
-else if(a<9)
-{
-    alert("you are a kid and you cannot even think of driving");
-}
+// else if(a<9)
+// {
+//     alert("you are a kid and you cannot even think of driving");
+// }
 
-else{
-    alert("you can now drive as you are above 18");
-}
+// else{
+//     alert("you can now drive as you are above 18");
+// }
 
-console.log("done")
+// console.log("done")
+
+console.log("you can",(a<18? "not drive":"drive"))
 
---
+++ tut7.js
@@ -0,0 +1,10 @@
+// chapter 2 pratice set
+
+// problem no1
+
+let age = 18;
+
+if(age>10 && age<=20){
+
+    console.log("your age lies between 10 and 20")
+}
