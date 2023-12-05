927f1d099185526a58b3b0866c6231157ce6e090
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-05 21:11:17 +0530

    this code for arrays compare ,reverse,

---
+++ index11.js
@@ -0,0 +1,46 @@
+//arrays 3rd methods
+
+// let num=[1,2,3,4,5,6,7,8,9]
+
+// let num_more=[11,12,13,14,15,16,17,18,19]
+
+// let num_even=[112,34,567,789,987,456,345,]
+
+// // console.log(num.length)
+
+
+
+// let newArray = num.concat( num_more,num_even)
+
+// console.log(newArray)
+// console.log(num,num_more)
+
+
+// sort method
+//  let compare =(a,b)=>{
+//     // return a-b
+//     return a-b
+//  }
+
+// let num=[12,45,5,6,17,67,9,7,909]
+
+
+// num.sort(compare)
+// num.reverse()
+// console.log(num)
+
+// splice and slice
+
+let number=[12,234,45,56,978,87,87,65,84,24]
+let deletevalues= number.splice(2,3,1022,1023,1024,)
+// console.log(num)
+console.log(deletevalues)
+
+// let newnumber= number.slice(3,5)
+// console.log(newnumber)
+
+
+
+
+
+
