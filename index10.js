592d9e3e878f8c30740d1f097e595dc58024e10b
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-05 20:17:23 +0530

    this code push pop shift,unshift methods of arrays

---
+++ index10.js
@@ -0,0 +1,31 @@
+// arrays method
+
+
+let num=[1,2,3,34,4,5]
+
+let b= num.toString() //b is now a string
+
+console.log( b,typeof b)
+
+let c=num.join("and")
+console.log(c,typeof c)
+
+let r = num.pop()  //pop returns the popped element
+
+console.log(num,r)
+
+let a=num.push(78) //push retuns the new array length
+
+console.log(num, a)
+
+
+// let d = num.shift()  //remove the element from start of the array
+
+// console.log(b,num)
+
+
+let  d =num.unshift(24)
+
+console.log(d,num)
+
+console.log(d)
